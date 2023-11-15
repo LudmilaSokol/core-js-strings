36d1ced0dc4bdfe5ab187254f53ebc3f0f0a40fd
LudmilaSokol <sluda999@yandex.ru>
2023-11-15 12:40:47 +0500

    feat: Add a solution to problems 12-15

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -236,11 +236,12 @@ function sumOfCodes(str) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
+  // throw new Error('Not implemented');
 }
 
-/**
+/** 13
  * Checks if a string ends with a specific substring.
  *
  * @param {string} str - The input string.
@@ -251,11 +252,12 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
+  // throw new Error('Not implemented');
 }
 
-/**
+/** 14
  * Returns a time string in the "mm:ss" format.
  *
  * @param {number} minutes - The number of minutes (non-negative integer).
@@ -268,11 +270,16 @@ function endsWith(/* str, substr */) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  let result = '';
+  const newMinutes = minutes.toString().padStart(2, '0');
+  const newSeconds = seconds.toString().padStart(2, '0');
+  result = newMinutes.concat(':', newSeconds);
+  return result;
+  // throw new Error('Not implemented');
 }
 
-/**
+/** 15
  * Returns a string in reverse order.
  *
  * @param {string} str - The input string.
@@ -282,8 +289,13 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    result = str[i] + result;
+  }
+  return result;
+  // throw new Error('Not implemented');
 }
 
 /**
